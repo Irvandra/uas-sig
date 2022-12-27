5c2487062c8ffd6b295e5af24c141efe2a3f4d0e
Irvandra <83211382+Irvandra@users.noreply.github.com>
2022-12-27 11:18:07 +0700

    Membuat peta tematik

--- app/Controllers/Maps.php
+++ app/Controllers/Maps.php
@@ -6,16 +6,34 @@ class Maps extends BaseController
 {
     public function index()
     {
+        $sumberDayaAlamModel = new \App\Models\SumberDayaAlamModel();
+
         $fileName = base_url('maps/map.geojson');
         $file = file_get_contents($fileName);
         $file = json_decode($file);
 
         $features = $file->features;
 
+        foreach($features as $index=>$feature)
+        {
+            $kode_wilayah = $feature->properties->kode;
+            $sumberDayaAlam = $sumberDayaAlamModel->where('id_master_data', 1)
+                    ->where('kode_wilayah', $kode_wilayah)
+                    ->first();
+
+            if ($sumberDayaAlam) {
+                $features[$index]->properties->jenis_sumber_daya = $sumberDayaAlam->jenis_sumber_daya;
+                $features[$index]->properties->kondisi = $sumberDayaAlam->kondisi;
+                $features[$index]->properties->nilai = $sumberDayaAlam->ketersediaan;
+                $nilaiMax = $sumberDayaAlamModel->select('MAX(ketersediaan) AS ketersediaan')->where('id_master_data', 1)->first()->ketersediaan;
+            }
+        }
+
         return view('maps/index', [
             'title' => 'Halaman Peta',
             'category' => 'Menu Halaman Peta',
-            'features' => $features
+            'features' => $features,
+            'nilaiMax' => $nilaiMax
         ]);
     }
 }
--- app/Views/maps/index.php
+++ app/Views/maps/index.php
@@ -17,7 +17,35 @@
 <?= $this->section('script'); ?>
 <script>
     var features = <?= json_encode($features) ?>;
+    var nilaiMax = <?= $nilaiMax ?>;
+
     var map = L.map('maps').setView({ lat : -5.3971, lon : 105.2668}, 11);
+
+    function getColor(d) {
+        return d > (nilaiMax/8)*7 ? '#800026' :
+        d > (nilaiMax/8)*6 ? '#BD0026' :
+        d > (nilaiMax/8)*5 ? '#E31A1C' :
+        d > (nilaiMax/8)*4 ? '#FC4E2A' :
+        d > (nilaiMax/8)*3 ? '#FDBD3C' :
+        d > (nilaiMax/8)*2 ? '#FEB24C' :
+        d > (nilaiMax/8)*1 ? '#FED976' :
+                            '#FFEDA0'
+    }
+
+    function style(feature) {
+       return{
+        weight : 2,
+        opacity : 1,
+        color : 'white',
+        dashArray : '3',
+        fillOpacity : 0.7,
+        fillColor : getColor(parseInt(feature.properties.nilai))
+       }; 
+    }
+
+    function onEachFeature(feature, layer){
+        layer.bindPopup("<h4>Sumber Daya Alam</h4><br>"+"Kecamatan : "+feature.properties.nama+"<br>"+"Jenis Sumber Daya : "+feature.properties.jenis_sumber_daya+"<br>Kondisi : "+feature.properties.kondisi+"<br>Ketersediaan : "+feature.properties.nilai);
+    }
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -26,6 +54,9 @@
 
     L.marker({ lat : -5.3971, lon : 105.2668}).bindPopup('Bandar Lampung').addTo(map);
 
-    var geojson = L.geoJson(features).addTo(map);
+    var geojson = L.geoJson(features, {
+        style: style,
+        onEachFeature: onEachFeature,
+    }).addTo(map);
 </script>
 <?= $this->endSection(); ?>
